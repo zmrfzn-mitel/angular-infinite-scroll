df70c2c3c881e15d46c8274537f83de89103c403
zameer fouzan <zameer.fouzan@mitel.com>
2020-11-26 19:40:57 +0530

    multiple api call, card update

--- src/app/_service/fact.service.ts
+++ src/app/_service/fact.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Fact } from '../_model/fact.model';
-import { Observable } from 'rxjs';
+import { forkJoin, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,7 @@ export class FactService {
 
   constructor(private http: HttpClient) { }
 
-  getRandomFact(): Observable<Fact[]> {
+  getRandomFact(start?: number, end?: number): Observable<Fact[]> {
     const month = Math.floor(Math.random() * 11) + 1;
     let maxDay = 30;
     if (month === 2) {
@@ -19,6 +19,13 @@ export class FactService {
       maxDay = 29;
     }
     const day = Math.floor(Math.random() * maxDay) + 1;
-    return this.http.get<Fact[]>(`http://numbersapi.com/${month}/${day}/date?json`);
+
+    let requestObj =[];
+    for(let i = start; i<=end; i++) {
+        requestObj.push(
+          this.http.get<Fact[]>(`http://numbersapi.com/${month}/${i}/date?json`)
+        );
+    }
+    return forkJoin(requestObj);
   }
 }
--- src/app/fact-scroller/fact-scroller.component.ts
+++ src/app/fact-scroller/fact-scroller.component.ts
@@ -25,44 +25,77 @@ export class FactsDataSource extends DataSource<Fact | undefined> {
   private dataStream = new BehaviorSubject<(Fact | undefined)[]>(this.cachedFacts);
   private subscription = new Subscription();
 
+  loading = false;
   private pageSize = 10;
   private lastPage = 0;
+  private pageStart = 1;
+  private pageEnd = this.pageStart + this.pageSize;
+  private totalItemsCount = 50;
+
+  private startIndex = 1;
+  // ($startIndex - 1) / $itemsPerPage) + 1;
 
   constructor(private factService: FactService) {
     super();
 
     // Start with some data.
-    this._fetchFactPage();
+    this._fetchFactPage(this.pageStart,this.pageEnd);
   }
 
   connect(collectionViewer: CollectionViewer): Observable<(Fact | undefined)[] | ReadonlyArray<Fact | undefined>> {
     this.subscription.add(collectionViewer.viewChange.subscribe(range => {
 
+
+
+      // console.log('range', range);
       const currentPage = this._getPageForIndex(range.end);
 
+      const totalItemsCount = 50;
+
+      this.getPaginationText(totalItemsCount, this.pageSize, currentPage);
+
       if (currentPage && range) {
-        console.log(currentPage, this.lastPage);
+        console.log('page:', currentPage, this.lastPage);
       }
 
       if (currentPage > this.lastPage) {
+        this.loading = true;
+
+
+        this.getPaginationText(this.totalItemsCount, this.pageSize, currentPage);
         this.lastPage = currentPage;
-        this._fetchFactPage();
+        if (this.pageEnd <= this.totalItemsCount) {
+          this._fetchFactPage(this.pageStart, this.pageEnd);
+          const event = { first: this.pageStart, rows: this.pageSize }
+          // call here
+          // this.myCustomFunc(event);
+        }
       }
     }));
     return this.dataStream;
+  }
+
+  getPaginationText(totalItemsCount, numberOfItemsPerPage, page) {
+    var pagesCount = (totalItemsCount - 1) / numberOfItemsPerPage + 1;
+    var start = (page - 1) * numberOfItemsPerPage + 1;
+    var end = Math.min(start + numberOfItemsPerPage - 1, totalItemsCount);
+
+    console.log('page:', `${start} to ${end} of ${totalItemsCount}`);
+    return `${start} to ${end} of ${totalItemsCount}`;
   }
 
   disconnect(collectionViewer: CollectionViewer): void {
     this.subscription.unsubscribe();
   }
 
-  private _fetchFactPage(): void {
-    for (let i = 0; i < this.pageSize; ++i) {
-      this.factService.getRandomFact().subscribe(res => {
-        this.cachedFacts = this.cachedFacts.concat(res);
-        this.dataStream.next(this.cachedFacts);
-      });
-    }
+  private _fetchFactPage(start?, end?): void {
+    // for (let i = 0; i < this.pageSize; ++i) {
+    this.factService.getRandomFact(start, end).subscribe(res => {
+      this.cachedFacts = this.cachedFacts.concat(res);
+      this.dataStream.next(this.cachedFacts);
+    });
+    // }
+    this.loading = false;
   }
 
   private _getPageForIndex(i: number): number {
